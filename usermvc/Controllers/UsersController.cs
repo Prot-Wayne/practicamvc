ca1260fe217bde04eceaf3debcd573992f224885
Leonardo Guerrero <guerrero8315@hotmail.com>
2018-04-25 20:43:51 -0500

    mvc

---
+++ usermvc/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using usermvc.Models;
+
+
+
+namespace usermvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            var db = new practicaEntities();
+            List<usuario> users = db.usuarios.ToList();
+            return View(users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(usuario u)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                var db = new practicaEntities();
+                db.usuarios.Add(u);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+
+                ModelState.AddModelError("", "Error al adicionar registro " + e.Message);
+                return View();
+            }
+        }
+    }
+}
---
+++ usermvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="usermvc.MvcApplication" Language="C#" %>
